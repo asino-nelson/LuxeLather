be187f73a7614cc1e67c1af09271da2e298b4b78
asino-nelson <asinonelson276@gmail.com>
2024-01-06 18:42:48 +0300

    Product card commit

--- luxe-lather/src/app/page.tsx
+++ luxe-lather/src/app/page.tsx
@@ -1,5 +1,8 @@
 import Banner from "./components/Banner";
 import Container from "./components/Container";
+import ProductCard from "./components/products/ProductCard";
+import { products } from "./utils/products";
+import { truncateText } from "./utils/truncate";
 
 export default function Home() {
   return (
@@ -8,7 +11,13 @@ export default function Home() {
       <div>
        <Banner/>
       </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
+        {products.map((product: any) => {
+          return <ProductCard data={product}/>;
+         }) }
+      </div>
     </Container>
+
     
   </div>
   );

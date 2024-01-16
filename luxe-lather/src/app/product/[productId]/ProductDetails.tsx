a7d5b4d9d6e133734b24db82cfbb3afa0b02ff08
asino-nelson <asinonelson276@gmail.com>
2024-01-16 21:39:21 +0300

    updated product details ui commit

--- luxe-lather/src/app/product/[productId]/ProductDetails.tsx
+++ luxe-lather/src/app/product/[productId]/ProductDetails.tsx
@@ -5,7 +5,9 @@ import ProductImage from "@/app/components/products/ProductImage";
 import SetQuantity from "@/app/components/products/SetQuantity";
 import { useCart } from "@/hooks/useCart";
 import { Rating } from "@mui/material";
+import { useRouter } from "next/navigation";
 import { useCallback, useEffect, useState } from "react";
+import { MdCheckCircle } from "react-icons/md";
 
 interface ProductDetailProps{
     product: any
@@ -40,6 +42,7 @@ const ProductDetails:React.FC<ProductDetailProps> = ({ product }) => {
       price: product.price
     }
   )
+  const router = useRouter()
 
   console.log(cartProducts)
 
@@ -93,19 +96,35 @@ const ProductDetails:React.FC<ProductDetailProps> = ({ product }) => {
           <div className={product.inStock ? "text-green-500 font-semibold" : "text-rose-500 font-semibold"}>
             {product.inStock ? "In Stock" : "Out of stock"}
           </div>
-          <div className="mt-4">
-            <SetQuantity
-             cartProduct={cartProduct}
-             handleQtyDecrease={handleQtyDecrease}
-             handleQtyIncrease={handleQtyIncrease}
-            />
-          </div>
-          <div className="max-w-[300px] mt-4">
-            <AddTocartBtn 
-              label="Add To Cart"
-              onClick={() => handleAddProductToCart(cartProduct)}
-            />
-          </div>
+          {isProductInCart ? (
+          <> 
+            <p className="mt-4 flex items-center gap-1">
+              <MdCheckCircle className="text-teal-400" size={24} />
+              <span>Product added to cart</span>
+            </p>
+            <div className="max-w-[300px] mt-2">
+              <AddTocartBtn outline label="View Cart" onClick={() => {
+                router.push("/cart")
+              }}/>
+            </div>
+          </>
+          ) : (
+          <>
+            <div className="mt-4">
+              <SetQuantity
+                cartProduct={cartProduct}
+                handleQtyDecrease={handleQtyDecrease}
+                handleQtyIncrease={handleQtyIncrease}
+              />
+            </div>
+            <div className="max-w-[300px] mt-4">
+              <AddTocartBtn 
+                label="Add To Cart"
+                onClick={() => handleAddProductToCart(cartProduct)}
+              />
+            </div>
+          </>)}
+         
           
         </div>
     </div>

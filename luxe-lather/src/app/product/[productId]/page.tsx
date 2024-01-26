2d513ff61303d52bbb59513c531b5696ad61f899
asino-nelson <asinonelson276@gmail.com>
2024-01-26 21:48:06 +0300

    individual items select commit

--- luxe-lather/src/app/product/[productId]/page.tsx
+++ luxe-lather/src/app/product/[productId]/page.tsx
@@ -1,7 +1,7 @@
 import Container from "@/app/components/Container"
 import ProductDetails from "./ProductDetails"
-import { product } from "@/app/utils/product"
 import ListRating from "./ListRating";
+import { products } from "@/app/utils/products";
 
 interface IParams {
     productId ?: string;
@@ -10,6 +10,8 @@ interface IParams {
 const Product = ({params}: {params:IParams}) => {
 
     console.log("params", params)
+
+    const product = products.find((item)=> item.id == params.productId )
 
   return (
     <div className="p-8">

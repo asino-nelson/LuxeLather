c08133f59543311d8bd8d684dce0ad57454a9cbc
asino-nelson <asinonelson276@gmail.com>
2024-01-29 22:13:47 +0300

    input component commit

--- luxe-lather/src/app/components/inputs/Input.tsx
+++ luxe-lather/src/app/components/inputs/Input.tsx
@@ -23,8 +23,20 @@ const Input:React.FC<InputProps> = ({
 }) => {
   return (
     <div className="w-full relative">
-      <input className=""/>
-      <label htmlFor={id}>{label}</label>
+      <input 
+        autoComplete="off"
+        id={id}
+        disabled={disabled}
+        {...register(id,{required})}
+        placeholder=""
+        type={type}
+        className={`peer w-full p-4 pt-6 outline-none border-y-white font-light border-2 rounded-md 
+          transition disabled:opacity-70 disabled:cursor-not-allowed
+          ${errors[id] ? "border-rose-400":"border-slate-300"}
+          ${errors[id] ? "focus:border-rose-400" : "focus:border-slate-300"}`} />
+      <label htmlFor={id} className="absolute cursor-text text-md duration-150 
+      transform -translate-y-3 top-5 z-10 origin-[0] left-4 peer-placeholder-shown:scale-100
+      peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:translate-y-4">{label}</label>
     </div>
   )
 }
